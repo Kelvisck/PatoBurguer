23910d2be2ee418bdc626fbb6d5ea7b5345ff8e1
kelvisck2 <kelvisck.cabral@ufms.br>
2023-07-17 17:06:00 -0400

    feat: inicializando

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Color minhaCor = const Color(0xFF9B0D);
+    return MaterialApp(
+      home: Directionality(
+        textDirection: TextDirection.ltr, // Defina a direcionalidade aqui
+        child: Scaffold(
+          backgroundColor: minhaCor,
+          body: Center(
+            child: Column(
+              children: const [CircularProgressIndicator()],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
