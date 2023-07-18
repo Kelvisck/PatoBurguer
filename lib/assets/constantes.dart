15ca13fc90b2d912be0baa35842ff2b3c8e7b8ce
kelvisck2 <kelvisck.cabral@ufms.br>
2023-07-17 21:54:04 -0400

    feat: adicionada a classe das constantes

---
+++ lib/assets/constantes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constentes {
+  //cores
+  static const Color corFundo = Color(0xFFFF9B0D);
+  static const Color CorfundoBranca = Color(0xFFFFFFFF);
+
+  //textos
+}
