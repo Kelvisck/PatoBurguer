60107cfd63f32243b92868557d1d9744d582bce8
kelvisck2 <kelvisck.cabral@ufms.br>
2023-07-18 18:59:40 -0400

    feat: tela do adm

--- lib/adm_page.dart
+++ lib/adm_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import './assets/constantes.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+//import 'package:google_sign_in/google_sign_in.dart';
 
 class AdmPage extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -10,7 +10,7 @@ class AdmPage extends StatelessWidget {
       children: [
         Positioned(
           top: 52,
-          left: 126,
+          left: 116,
           child: Text(Constantes.textoTitulo,
               style: TextStyle(
                   fontFamily: 'Roboto',
@@ -19,7 +19,7 @@ class AdmPage extends StatelessWidget {
         ),
         Positioned(
             top: 52,
-            left: 186,
+            left: 176,
             child: Text(Constantes.textoTitulo2,
                 style: TextStyle(
                     fontFamily: Constantes.fonteRoboto,
@@ -35,7 +35,7 @@ class AdmPage extends StatelessWidget {
                 fontWeight: FontWeight.bold),
           ),
           top: 116,
-          left: 20,
+          left: 34,
         ),
         Positioned(
           child: Text(
@@ -46,26 +46,32 @@ class AdmPage extends StatelessWidget {
                 fontWeight: FontWeight.bold),
           ),
           top: 144,
-          left: 20,
+          left: 34,
         ),
         Positioned(
-          top: 222,
+          top: 232,
           left: 70,
           child: ElevatedButton(
             onPressed: () {},
             style: ButtonStyle(
-              fixedSize: MaterialStateProperty.all<Size>(Size(257, 47)),
-              backgroundColor:
-                  MaterialStateProperty.all<Color>(Constantes.CorfundoBranca),
-              elevation: MaterialStateProperty.all<double>(10),
-            ),
+                fixedSize: MaterialStateProperty.all<Size>(Size(240, 50)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Constantes.CorfundoBranca),
+                elevation: MaterialStateProperty.all<double>(10),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ))),
             child: Align(
               alignment: Alignment.centerLeft, // Define o alinhamento do texto
               child: Row(
                 children: [
                   Text(
                     'Alterar Cardápio',
-                    style: TextStyle(color: Constantes.corFundo),
+                    style: TextStyle(
+                        color: Constantes.corFundo,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w800),
                   ),
                 ],
               ),
@@ -73,17 +79,140 @@ class AdmPage extends StatelessWidget {
           ),
         ),
         Positioned(
-          child: Image.asset("assets/recursos/cardapio.png"),
-          top: 300,
-          left: 50,
-        )
+          top: 235,
+          left: 246,
+          child: ColorFiltered(
+            colorFilter:
+                ColorFilter.mode(Constantes.corFundo, BlendMode.srcATop),
+            child: Image.asset(
+              "lib/assets/recursos/cardapio.png",
+              height: 40,
+              width: 50,
+            ),
+          ),
+        ),
+
+        // ----------- botao 1 --------------------------------------------------//
+        Positioned(
+          top: 312,
+          left: 70,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all<Size>(Size(240, 50)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Constantes.CorfundoBranca),
+                elevation: MaterialStateProperty.all<double>(10),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ))),
+            child: Align(
+              alignment: Alignment.centerLeft, // Define o alinhamento do texto
+              child: Row(
+                children: [
+                  Text(
+                    'Alterar Contato',
+                    style: TextStyle(
+                        color: Constantes.corFundo,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w800),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 316,
+          left: 248,
+          child: ColorFiltered(
+            colorFilter:
+                ColorFilter.mode(Constantes.corFundo, BlendMode.srcATop),
+            child: Image.asset(
+              "lib/assets/recursos/contato.png",
+              height: 40,
+              width: 50,
+            ),
+          ),
+        ),
+
+        // botao 2 /\ -------------------------------------------------------------//
+
+        Positioned(
+          top: 392,
+          left: 70,
+          child: ElevatedButton(
+            onPressed: () {},
+            style: ButtonStyle(
+                fixedSize: MaterialStateProperty.all<Size>(Size(240, 50)),
+                backgroundColor:
+                    MaterialStateProperty.all<Color>(Constantes.CorfundoBranca),
+                elevation: MaterialStateProperty.all<double>(10),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ))),
+            child: Align(
+              alignment: Alignment.centerLeft, // Define o alinhamento do texto
+              child: Row(
+                children: [
+                  Text(
+                    'Alterar Senha',
+                    style: TextStyle(
+                        color: Constantes.corFundo,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w800),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 398,
+          left: 248,
+          child: ColorFiltered(
+            colorFilter:
+                ColorFilter.mode(Constantes.corFundo, BlendMode.srcATop),
+            child: Image.asset(
+              "lib/assets/recursos/config.png",
+              height: 40,
+              width: 50,
+            ),
+          ),
+        ),
+
+        //----------------botao 3 /\---------------------------
       ],
     ));
   }
 }
 
+/*ColorFiltered(
+  colorFilter: ColorFilter.matrix([
+    // Red
+    0.0, 0.0, 0.0, 0.0, 0.0,
+    // Green
+    0.0, 0.0, 0.0, 0.0, 0.0,
+    // Blue
+    0.0, 0.0, 0.0, 0.0, 1.0,
+    // Alpha
+    0.0, 0.0, 0.0, 1.0, 0.0,
+  ]),
+  child: Image.asset('caminho_da_sua_imagem'),
+)*/
 
-/*child: Text(
-                'Alterar Cardápio',
-                style: TextStyle(color: Constantes.corFundo),
-              )*/
+
+/*child: Image.asset(
+            "lib/assets/recursos/cardapio.png",
+            height: 40,
+            width: 50,
+          ),
+          top: 225,
+          left: 266,
+        )
+      ],
+    ));
+  }
+}*/
--- lib/assets/constantes.dart
+++ lib/assets/constantes.dart
@@ -10,4 +10,9 @@ class Constantes {
   static const String textoTitulo = 'Pato ';
   static const String textoTitulo2 = 'Burguer';
   static const String fonteRoboto = 'Roboto';
+
+  static int get g_red => corFundo.red;
+  static int get g_green => corFundo.green;
+  static int get g_blue => corFundo.blue;
+  static int get g_alpha => corFundo.alpha;
 }
