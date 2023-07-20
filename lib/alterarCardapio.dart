3fbbe998a0981709cc48d7c8febee55af083843e
kelvisck2 <kelvisck.cabral@ufms.br>
2023-07-19 21:49:42 -0400

    feat:correção layout

---
+++ lib/alterarCardapio.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:pato_burguer/assets/constantes.dart';
+
+class AlterarCardapio extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Alterar Cardápio'),
+          centerTitle: true,
+          backgroundColor: Constantes.corFundo,
+        ),
+        backgroundColor: Color(0xFFFF9B0D),
+        body: Container());
+  }
+}
