d29594078d562fb8540af3312a1db50bd36aa73b
CbumDaddy <pedro_alencar@ufms.br>
2023-07-23 19:45:51 -0300

    feat: Deu muita merda mas desfiz

--- lib/log_page.dart
+++ lib/log_page.dart
@@ -7,14 +7,36 @@ class logPage extends StatelessWidget {
     // TODO: implement build
     return Scaffold(
       backgroundColor: Color(0xFFFF9B0D),
+      body: Stack(children: [
+        Container(
+              margin: EdgeInsets.only(top: 154),
+              child:
+              Positioned(
+                top: 140,
+                left: -20,
+                child: SingleChildScrollView(
+                  child: Login(),
+                ))
+       )]),
+    );
+  }
+}
+
+
+
+
+/* import 'package:flutter/material.dart';
+import 'package:pato_burguer/TelaLog.dart';
+import 'package:pato_burguer/telaLog.dart';
+
+class logPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      backgroundColor: Color(0xFFFF9B0D),
       body: Stack(
         children: [
-          /*Positioned(
-        top: 60,
-        left: 95,
-        child: Image.asset("lib/assets/recursos/icone_app_admin.png", width: 206, height: 189,
-          ),
-        ),*/
         Stack(
           children:[
             Container(
@@ -27,30 +49,10 @@ class logPage extends StatelessWidget {
                   child: Login(),
                 ))
             )
-      ]),
-        ],
+        ]
+      ),
+      ],
       )
     );
   }
-}
-
-/*Positioned(
-        top: 60,
-        left: 95,
-        child: Image.asset("lib/assets/recursos/icone_app_admin.png", width: 206, height: 189,
-          ),
-        ), */
-
-/*Stack(
-        children:[
-        Container(
-          margin: EdgeInsets.only(top: 154),
-          child:
-          Positioned(
-          top: 140,
-          left: -20,
-          child: SingleChildScrollView(
-            child: Login(),
-        ))
-        )
-      ]), */
+}*/
