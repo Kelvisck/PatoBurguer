a2602d8a7963bb6eb03a871ff48dfb90e45bf35b
CbumDaddy <pedro_alencar@ufms.br>
2023-08-01 19:17:37 -0300

    feat: reset pw alterações de ui

--- lib/reset-password-page.dart
+++ lib/reset-password-page.dart
@@ -1,222 +1,145 @@
 import 'package:flutter/material.dart';
 import 'package:pato_burguer/assets/constantes.dart';
 import 'package:pato_burguer/assets/widgetsFunctions.dart';
-class ResetPassword extends StatefulWidget {
-  const ResetPassword({super.key});
 
-  @override
-  State<ResetPassword> createState() => _ResetPasswordState();
-}
-
-class _ResetPasswordState extends State<ResetPassword> {
-
-  bool _showpassord = false;
+class ResetPassoword extends StatelessWidget {
+  const ResetPassoword({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(Constantes.corFundo.value),
-      body: Stack(
-        
-        children: [
-          Positioned(
-            top: 22,
-            left: 3,
-            child:
-              IconButton(
-                onPressed: ()=> Navigator.pop(context, false), 
-                icon: Icon(
-                  Icons.arrow_back_ios_new_rounded, 
-                  color: Colors.white,
-                  )
-                ),
-                ),
-          Positioned(
+      body: Stack(children: [
+        AppBar(
+          backgroundColor: Color(Constantes.corFundo.value),
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context, false),
+              icon: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: Colors.white,
+              )),
+          title: Padding(
+            padding: EdgeInsets.all(15.0),
             child: Text(
-              'Alterar Senha',
+              'Recuperação de Senha',
               style: TextStyle(
                   fontFamily: 'Roboto',
                   color: Colors.white,
                   fontWeight: FontWeight.w900,
                   fontSize: 24),
             ),
-            top: 32,
-            left: 120,
           ),
-          Positioned(
-              top: 110,
-              left: 23,
-              //container de fundo ------------------------------------------------------------------
-              child: 
-               Padding(
-                padding: const EdgeInsets.only(top: 30, bottom: 30),
-                child: Container(
-                  width: 350,
-                  height: 450,
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(18),
-                          topRight: Radius.circular(18),
-                          bottomLeft: Radius.circular(18),
-                          bottomRight: Radius.circular(18)
-                      )
-                    ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
+        ),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 30, bottom: 30),
+            child: Container(
+                width: 375,
+                height: 450,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(18),
+                        topRight: Radius.circular(18),
+                        bottomLeft: Radius.circular(18),
+                        bottomRight: Radius.circular(18))),
+                child: ListView(
+                  padding: const EdgeInsets.only(
+                    left: 40,
+                    right: 40,
+                  ),
+                  children: [
+                    Column(
                       children: [
                         SizedBox(
-                          height: 40,
+                          height: 200,
+                          width: 200,
+                          child: Image.asset(
+                            "lib/assets/recursos/pato-bolado-senha.png",
+                          ),
                         ),
                         Align(
-                          alignment: 
-                            Alignment(-1, 0),
-                          child: 
-                            textoSimples("Senha Atual", 21),
+                          alignment: Alignment(0, 0),
+                          child: textoSimples("Esqueceu sua Senha?", 30),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Align(
+                          alignment: Alignment(0, 0),
+                          child: Text(
+                            "Insira seu E-mail logado em sua conta para podermos lhe enviar um link com instruções sobre a recuperação de senha",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 14,
+                              fontFamily: 'Roboto',
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
                         ),
                         TextFormField(
                           autofocus: true,
                           style: new TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontFamily: Constantes.fonteRoboto,
-                              ),
-                            decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(7))
-                              ),
-                              enabledBorder: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(7))
-                              ),
-                              //labelText: "Senha Atual",
-                              labelStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontFamily: Constantes.fonteRoboto,
+                          ),
+                          decoration: InputDecoration(
+                            contentPadding: EdgeInsets.symmetric(
+                                horizontal: 16.0, vertical: 12.0),
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7))),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(7))),
+                            hintText: "E-mail",
+                            labelStyle: TextStyle(
                                 color: Colors.black,
                                 fontFamily: 'Roboto',
-                              ),
-                              suffixIcon: GestureDetector(
-                                child: Icon(_showpassord == false ? Icons.visibility_off: Icons.visibility,color: Colors.black,),
-                                onTap: () {
-                                  setState((){
-                                   _showpassord = !_showpassord;
-                                  });
-                                },
-                              )
-                            ),
-                              obscureText: _showpassord == false ? true: false,
+                                fontWeight: FontWeight.w400),
                           ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Align(
-                            alignment: 
-                              Alignment(-1, 0),
-                            child: 
-                              textoSimples("Nova Senha", 21),
-                          ),
-                          TextFormField(
-                            autofocus: true,
-                            style: new TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontFamily: Constantes.fonteRoboto,
-                              ),
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-                                border: OutlineInputBorder(
-                                borderRadius: BorderRadius.all(Radius.circular(7))
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(7))
-                                ),
-                                //labelText: "Nova Senha",
-                                labelStyle: TextStyle(
-                                  color: Colors.black,
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                                minimumSize:
+                                    MaterialStateProperty.all(Size(355, 50)),
+                                shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                    RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(8),
+                                )),
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                        Constantes.corFundo)),
+                            onPressed: () {},
+                            child: Text(
+                              'Enviar',
+                              style: TextStyle(
+                                  fontSize: 22,
                                   fontFamily: 'Roboto',
-                                  ),
-                                suffixIcon: GestureDetector(
-                                  child: Icon(_showpassord == false ? Icons.visibility_off: Icons.visibility,color: Colors.black,),
-                                  onTap: () {
-                                    setState((){
-                                      _showpassord = !_showpassord;
-                                    });
-                                  },
-                                )
-                              ),
-                              obscureText: _showpassord == false ? true: false,
-                          ),
-                          SizedBox(
-                            height: 10,
+                                  fontWeight: FontWeight.w600),
                             ),
-                          Align(
-                            alignment: 
-                              Alignment(-1, 0),
-                            child: 
-                              textoSimples("Confirmar Senha", 21),
                           ),
-                          TextFormField(
-                            autofocus: true,
-                            style: new TextStyle(
-                                color: Colors.black,
-                                fontSize: 20,
-                                fontFamily: Constantes.fonteRoboto,
-                              ),
-                              decoration: InputDecoration(
-                                contentPadding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 12.0),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(7))
-                                ),
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius: BorderRadius.all(Radius.circular(7))
-                                ),
-                                //labelText: "Confirmar Senha",
-                                labelStyle: TextStyle(
-                                  color: Colors.black,
-                                  fontFamily: 'Roboto',
-                                ),
-                                suffixIcon: GestureDetector(
-                                  child: Icon(_showpassord == false ? Icons.visibility_off: Icons.visibility,color: Colors.black,),
-                                  onTap: () {
-                                    setState((){
-                                      _showpassord = !_showpassord;
-                                    });
-                                  },
-                                )
-                              ),
-                              obscureText: _showpassord == false ? true: false,
-                          ),
-                          SizedBox(
-                            height: 80,
-                          ),
-                          ElevatedButton(
-                                style: ButtonStyle(
-                                    minimumSize: MaterialStateProperty.all(
-                                        Size(355, 50)),
-                                    shape: MaterialStateProperty.all<
-                                            RoundedRectangleBorder>(
-                                        RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(8),
-                                    )),
-                                    backgroundColor:
-                                        MaterialStatePropertyAll<Color>(
-                                            Constantes.corFundo)),
-                                onPressed: () {},
-                                child: Text(
-                                  'Salvar',
-                                  style: TextStyle(
-                                      fontSize: 24, fontFamily: 'Poppins-Bold'),
-                                ),
-                              )
+                        ),
+                        SizedBox(
+                          height: 10,
+                        )
                       ],
                     ),
-                  )
-                ),
-              )
-              )
-              
-       ]
-      ),
-      );
+                  ],
+                )),
+          ),
+        ),
+      ]),
+    );
   }
 }
